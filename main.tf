8eebaa1f36b24aea30372dc715b859c550eae11d
lazaaro01 <lazaro13.vascon@gmail.com>
2025-12-05 21:11:35 -0300

    infra: Correções no meu main.tf e na Pipeline

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.6.0"
-  
+
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.6.0"
+      source  = "hashicorp/docker"
+      version = "latest"
     }
   }
 }
